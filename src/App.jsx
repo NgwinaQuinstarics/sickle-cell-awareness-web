4f9324f08120c4ab61f7920a961e735d8fe87756
quinstarics24 <quinngwina@gmail.com>
2026-03-05 07:03:46 +0100

    Initial React project setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Sickle Cell Awareness Platform</h1>
+      <p>Encouraging people to know their genotype.</p>
+    </div>
+  );
+}
+
+export default App;
